7a7a0f8d38eca5c0695b19064792531911cf0e74
Jonah Unuafe <fejisfejiro@gmail.com>
2024-07-24 15:15:01 +0100

    footer added

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='py-10 text-center'>
+        Copyright &copy; 2024 All rights reserved.
+        <br />
+        Coded by JonahDev.
+    </div>
+  )
+}
+
+export default Footer
