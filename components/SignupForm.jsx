04aab7f729c7c8f9b4c7778b990f337c5fe59e9e
Jonah Unuafe <fejisfejiro@gmail.com>
2024-07-24 14:46:11 +0100

    set up input dynamic input template

---
+++ components/SignupForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SignupForm = () => {
+  return (
+    <section className='container'>
+        <form className='border-2 border-paragraphColor rounded-lg max-w-sm mx-auto px8 py-6 space-y-5'>
+            <h2 className='text-center special-word'>Sign up</h2>
+
+            
+        </form>
+    </section>
+  )
+}
+
+export default SignupForm
