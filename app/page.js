e6631037bbc35220d32453f687982b1849bf8877
Jonah Unuafe <fejisfejiro@gmail.com>
2024-07-24 14:34:22 +0100

    populated Home Page with content

--- app/page.js
+++ app/page.js
@@ -1,7 +1,30 @@
+import Image from "next/image";
+import Jonah from "../public/image/jonah.jpeg"
+
 export default function Home() {
   return (
-    <div>
-      This is the homepage
+    <div className="container flex flex-col md:flex-row gap-5 h-[calc(100vh-4rem)]">
+      <div className="basis-full flex flex-col justify-center md:basis-2/3/">
+        <p className="special-word text-xs">Protect All the Birds</p>
+        <h1 className="pb-5">
+          The world's <span className="special-word">Rarest</span><br /> Birds
+        </h1>
+
+        <p>
+          This is one of the most rarest bird on the planet. This bird has the capacity to fly a 100ft
+          into the air without been exhausted. It hunts for prey excellently well. It has a weight of
+          approxiamtely 150lbs and height of 50ft.
+        </p>
+      </div>
+
+      <div className="hidden md:block basis-1/3">
+        <Image 
+          src={Jonah} 
+          alt="avatar"
+          sizes="100vw"
+          className="w-full h-auto"
+          />
+      </div>
     </div>
   );
 }
