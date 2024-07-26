53cbbc4ca524bde9256179f64058816087836b3e
Jonah Unuafe <fejisfejiro@gmail.com>
2024-07-26 23:55:19 +0100

    created function to fetch blogpost from MongoDB database

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 
-const Blog = () => {
+async function fetchBlogs() {
+  const res = await fetch("http://localhost:3000/api/blog", {
+    cache: "no-store"
+  });
+
+  if(!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const Blog = async () => {
+  const blogs = await fetchBlogs();
+  console.log(blogs);
   return (
     <div>Blog</div>
   )
