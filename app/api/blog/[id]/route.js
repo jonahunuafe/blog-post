c7effec9a7acdcdfd1010a3421666884a8bc89ac
Jonah Unuafe <fejisfejiro@gmail.com>
2024-07-27 01:59:27 +0100

    created dynamic api to update post

---
+++ app/api/blog/[id]/route.js
@@ -0,0 +1,71 @@
+// http://localhost:3000/api/blog
+
+
+import Blog from "@/models/Blog";
+import {connect} from "../../../lib/db"
+import { NextResponse } from "next/server";
+import { verifyJwtToken } from "@/lib/jwt";
+
+export async function PUT(req, res) {
+    await connect();
+
+    const id = res.params.id;
+
+    const accessToken = req.headers.get("authorization");
+    // [bearer, token]
+    const token = accessToken.split(" ")[1]
+
+    const decodedToken = verifyJwtToken(token);
+
+    if(!accessToken || !decodedToken) {
+        return NextResponse.json(
+            {error: "unathorized (wrong or expired token)"},
+            {status: 403}
+        );
+    }
+
+    try {
+        const body = await req.json();
+        const blog = Blog.findById(id).populate("authorId");
+
+        if(blog?.authorId?._id.toString() !== decodedToken._id.toString()) {
+            return NextResponse.json(
+                {msg: "Only author can update his/her blog"},
+                {status: 403}
+            )
+        }
+
+        const updateBlog = await Blog.findByIdAndUpdate(
+            id, 
+            {$set: {...body}},
+            {new: true}
+        );
+
+        return NextResponse.json(updateBlog, {status: 200});
+    } catch (error) {
+        return NextResponse.json({message: "PUT error"}, {status: 500})
+    }
+}
+
+
+export async function GET(req, res) {          //We get the id from the response
+    await connect();
+
+    const id = res.params.id;
+
+    try {
+        const blog = await Blog.findById(id).populate({
+            path: "authorId",
+            select: "password"
+        })
+        .populate({
+            path: "comments.user",
+            select: "password"
+        })
+
+        return NextResponse.json(blog, {status: 200});
+
+    } catch(error) {
+        return NextResponse.json({message: "GET error"}, {status: 500}) 
+    }
+}
