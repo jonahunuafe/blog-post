c57cbf89553fc1f0a5840bb34b1f7d229f8e6a4a
Jonah Unuafe <fejisfejiro@gmail.com>
2024-08-07 01:10:50 +0100

    updates

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import SignupForm from '@/components/SignupForm'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
-import { authOptions } from '../api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 const Signup = async () => {
   const session = await getServerSession(authOptions);
